5219edc24756bb22cc4ee1aac0d76a7886ef21c4
unknown <JMader8>
2020-02-27 09:38:25 -0600

    The files are there now

--- planetclass/planetclass.py
+++ planetclass/planetclass.py
@@ -1,8 +1,9 @@
 class Planet(object):
     planets = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune", "Pluto"]
 
-    def __init__(self, planets):
-        self.planetsIndex = planets
+    def __init__(self, name, order):
+        self.name = name
+        self.order = order
 
     def __init__(self) :
         planet = Planet.planets
